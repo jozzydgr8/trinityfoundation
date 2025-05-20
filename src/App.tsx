8ecf3887d7bb99872769ebbfde056341d2db4008
jozzydgr8 <jozzydgreat1@gmail.com>
2025-05-20 16:00:40 -0700

    added htaccess

--- src/App.tsx
+++ src/App.tsx
@@ -28,12 +28,12 @@ import StripeSuccess from './Pages/StripeSuccess'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDHAJYVLlNM8u2L-PMngG-oeJ9BpEw6AqA",
-  authDomain: "apartmentwebsite-fe584.firebaseapp.com",
-  projectId: "apartmentwebsite-fe584",
-  storageBucket: "apartmentwebsite-fe584.appspot.com",
-  messagingSenderId: "649191728575",
-  appId: "1:649191728575:web:020b8bf5025fb749a56d0a"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId, 
 };
 
 // Initialize Firebase
